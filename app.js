382dcf7f48d97b76cc120765dcdee29283050905
José Pedro Gomes de Lima <jpg.lima2011@gmail.com>
2025-10-06 20:30:16 -0300

    ajuste tela inicialexibir agenda e logo

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ function carregarPagina(pagina) {
       document.getElementById("conteudo").innerHTML = `
         <h1>Bem-vindos ao Ministério Mais Comunhão</h1>
         <p>Este é o nosso espaço para compartilhar fé, arte e amor.</p>
+  <h2>Confira a nossa agenda</h2>
+  <p>Dia 11 de outubro de 2025: evento na Arena de Pernambuco, às 13 horas.</p>
+
+  <h2>Álbum <strong>Mais Comunhão</strong></h2>
+  <p>
+    Confira as músicas do EP Mais Comunhão:<br>
+    <a href="https://open.spotify.com/intl-pt/track/6wn3ZDK4ahgIMW9BErWGVl?si=53db13e3c9924643">
+      Tocar no <strong>Spotify</strong>
+    </a><br>
+    <a href="https://youtu.be/PgAUuSSgSW8?si=3xJBpU90u1V7K7qi">
+      Tocar no <strong>YouTube</strong>
+    </a>
+  </p>
+
       `;
       break;
     case "poesias":
