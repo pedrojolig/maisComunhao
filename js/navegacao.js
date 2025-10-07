224a45fe61b6afd0b16e3402b63fe90f6b72e33f
José Pedro Gomes de Lima <jpg.lima2011@gmail.com>
2025-10-07 08:19:42 -0300

    ajuste para exibir corretamente a tela inicial ao clicar no botao voltar

--- js/navegacao.js
+++ js/navegacao.js
@@ -1,6 +1,7 @@
 export function mostrarMenuInicial() {
   const logo = document.getElementById("logo");
   const menuContainer = document.getElementById("menu");
+  const conteudo = document.getElementById("conteudo");
 
   if (logo) logo.style.display = "block";
 
@@ -14,5 +15,36 @@ export function mostrarMenuInicial() {
         <li><a href="#" onclick="abrirTela('faleConosco')">Fale Conosco</a></li>
       </ul>
     </nav>
+  `;
+
+  conteudo.innerHTML = `
+    <section id="boas-vindas">
+      <h1>Shalom! Seja muito bem-vindo ao Ministério Mais Comunhão</h1>
+      <p>
+        Que este espaço seja um lugar de paz, inspiração e comunhão com Deus. Aqui celebramos a fé, a arte e o amor que nos une como corpo de Cristo.
+      </p>
+      <blockquote>
+        “O Senhor te abençoe e te guarde;<br>
+        o Senhor faça resplandecer o Seu rosto sobre ti e tenha misericórdia de ti;<br>
+        o Senhor sobre ti levante o Seu rosto e te dê a paz.”<br>
+        <strong>— Números 6:24–26</strong>
+      </blockquote>
+    </section>
+
+    <h2>Confira nossa agenda.</h2>
+    <p>
+      Dia 11 de outubro de 2025: evento na Arena de Pernambuco, às 13 horas.
+    </p>
+
+    <h2>Álbum <strong>Mais Comunhão</strong></h2>
+    <p>
+      Confira as músicas do EP Mais Comunhão.<br>
+      <a href="https://open.spotify.com/intl-pt/track/6wn3ZDK4ahgIMW9BErWGVl?si=53db13e3c9924643">
+        Tocar Mais Comunhão no <strong>Spotify</strong>
+      </a><br>
+      <a href="https://youtu.be/PgAUuSSgSW8?si=3xJBpU90u1V7K7qi">
+        Tocar Mais Comunhão no <strong>YouTube</strong>
+      </a>
+    </p>
   `;
 }
