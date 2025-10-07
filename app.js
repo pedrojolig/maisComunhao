b52ce71b33b2207bf5c5c4f36f2dc1c7924c5594
José Pedro Gomes de Lima <jpg.lima2011@gmail.com>
2025-10-06 22:03:09 -0300

    ajuste para exibir a agenda e oslinksdo youtube e spotify na tela inicial e nao no menu

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ function mostrarMenuInicial() {
         <li><a href="#" onclick="abrirTela('loja')">Loja</a></li>
         <li><a href="#" onclick="abrirTela('quemSomos')">Quem Somos</a></li>
         <li><a href="#" onclick="abrirTela('faleConosco')">Fale Conosco</a></li>
-        <li><a href="#" onclick="abrirTela('agenda')">Agenda & Álbum</a></li>
       </ul>
     </nav>
   `;
@@ -26,6 +25,22 @@ function mostrarMenuInicial() {
   conteudo.innerHTML = `
     <h1>Bem-vindos ao Ministério Mais Comunhão</h1>
     <p>Este é o nosso espaço para compartilhar fé, arte e amor.</p>
+
+    <h2>Confira nossa agenda.</h2>
+    <p>
+      Dia 11 de outubro de 2025: evento na Arena de Pernambuco, às 13 horas.
+    </p>
+
+    <h2>Álbum <strong>Mais Comunhão</strong></h2>
+    <p>
+      Confira as músicas do EP Mais Comunhão.<br>
+      <a href="https://open.spotify.com/intl-pt/track/6wn3ZDK4ahgIMW9BErWGVl?si=53db13e3c9924643" target="_blank">
+        Tocar Mais Comunhão no <strong>Spotify</strong>
+      </a><br>
+      <a href="https://youtu.be/PgAUuSSgSW8?si=3xJBpU90u1V7K7qi" target="_blank">
+        Tocar Mais Comunhão no <strong>YouTube</strong>
+      </a>
+    </p>
   `;
 }
 
@@ -42,10 +57,7 @@ function abrirTela(tela) {
 
   switch (tela) {
     case "inicio":
-      conteudo.innerHTML = `
-        <h1>Bem-vindos ao Ministério Mais Comunhão</h1>
-        <p>Este é o nosso espaço para compartilhar fé, arte e amor.</p>
-      `;
+      mostrarMenuInicial();
       break;
 
     case "poesias":
@@ -113,26 +125,6 @@ function abrirTela(tela) {
           "Mensagem enviada com sucesso! Deus abençoe.";
         this.reset();
       });
-      break;
-
-    case "agenda":
-      conteudo.innerHTML = `
-        <h2>Confira nossa agenda.</h2>
-        <p>
-          Dia 11 de outubro de 2025: evento na Arena de Pernambuco, às 13 horas.
-        </p>
-
-        <h2>Álbum <strong>Mais Comunhão</strong></h2>
-        <p>
-          Confira as músicas do EP Mais Comunhão.<br>
-          <a href="https://open.spotify.com/intl-pt/track/6wn3ZDK4ahgIMW9BErWGVl?si=53db13e3c9924643" target="_blank">
-            Tocar Mais Comunhão no <strong>Spotify</strong>
-          </a><br>
-          <a href="https://youtu.be/PgAUuSSgSW8?si=3xJBpU90u1V7K7qi" target="_blank">
-            Tocar Mais Comunhão no <strong>YouTube</strong>
-          </a>
-        </p>
-      `;
       break;
   }
 }
