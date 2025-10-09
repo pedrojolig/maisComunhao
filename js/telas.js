8bd19b4113558a38d1f6b3bfdb06a96a37a4ba8a
José Pedro Gomes de Lima <jpg.lima2011@gmail.com>
2025-10-09 18:05:02 -0300

    criacao da tela albuns com as subtelas EP mais comunhao e CD entrega

--- js/telas.js
+++ js/telas.js
@@ -3,7 +3,7 @@ export const telas = {
     <h1>Poesias de Ketyanne</h1>
     <ul>
       <li>
-        <a href="https://youtu.be/oQ8BKHoMZ1Y" target="_blank">
+        <a href="https://youtu.be/oQ8BKHoMZ1nY" target="_blank">
           Assistir à poesia "Minha Identidade" no YouTube
         </a>
       </li>
@@ -34,22 +34,42 @@ export const telas = {
       </li>
     </ul>
   `,
+
   albuns: `
     <h1>Álbuns Mais Comunhão</h1>
-    <p>Confira as músicas do EP <strong>Mais Comunhão</strong> disponíveis nas plataformas:</p>
+
+    <h2>🎵 EP Mais Comunhão</h2>
     <ul>
       <li>
         <a href="https://open.spotify.com/intl-pt/track/6wn3ZDK4ahgIMW9BErWGVl?si=53db13e3c9924643" target="_blank">
-          Ouvir no Spotify
+          Ouvir "Mais Comunhão" no Spotify
         </a>
       </li>
       <li>
         <a href="https://youtu.be/PgAUuSSgSW8?si=3xJBpU90u1V7K7qi" target="_blank">
-          Ouvir no YouTube
+          Ouvir "Mais Comunhão" no YouTube
         </a>
       </li>
     </ul>
+
+    <h2>💿 CD Entrega</h2>
+    <ul>
+      <li><a href="https://youtu.be/VVxcTFSS1nQ" target="_blank">Assistir à poesia "Diante de Ti"</a></li>
+      <li><a href="https://youtu.be/tWkPo_8vYE4?si=qC4ckEZ7SiBErZoQ" target="_blank">Ouvir "Diante de Ti"</a></li>
+      <li><a href="https://youtu.be/kE1MT3IXsQI?si=1uVf_M78oDnFVmWP" target="_blank">Ouvir "Posso Crer"</a></li>
+      <li><a href="https://youtu.be/cOJNk8AlsQU" target="_blank">Ouvir "Eu Sou do Meu Amado"</a></li>
+      <li><a href="https://youtu.be/hxUvdvLorfg" target="_blank">Ouvir "Eu Nunca Desisti"</a></li>
+      <li><a href="https://youtu.be/a7kxv4Hd9vk" target="_blank">Ouvir "Até Quando"</a></li>
+      <li><a href="https://youtu.be/8QdAa6hYKnY" target="_blank">Ouvir "Basta Crer"</a></li>
+      <li><a href="https://youtu.be/7v1ie5lyJIA" target="_blank">Ouvir "Mesmo em Meio à Dor"</a></li>
+      <li><a href="https://youtu.be/SFTTA1Pp3JY" target="_blank">Ouvir "O Espírito de Deus"</a></li>
+      <li><a href="https://youtu.be/15tbDQ5F9xU" target="_blank">Ouvir "Vamos Celebrar"</a></li>
+      <li><a href="https://youtu.be/UukRJ5Ez7x0" target="_blank">Ouvir "Tão Grande Amor"</a></li>
+      <li><a href="https://youtu.be/bxJlS3clQNQ" target="_blank">Ouvir "Justiça Perfeita"</a></li>
+      <li><a href="https://youtu.be/RyTnL2ENUCY" target="_blank">Assistir à Ministração</a></li>
+    </ul>
   `,
+
   convite: `
     <h1>Convite para Ministrar</h1>
     <form id="form-convite">
@@ -64,6 +84,7 @@ export const telas = {
     </form>
     <div id="resposta-convite"></div>
   `,
+
   loja: `
     <h1>Loja</h1>
     <ul>
@@ -72,10 +93,12 @@ export const telas = {
       <li>Camisa do Ministério – R$ 49,90</li>
     </ul>
   `,
+
   quemSomos: `
     <h1>Quem Somos</h1>
     <p>Somos Ketyanne e Pedro Lima, fundadores do Ministério Mais Comunhão. Atuamos como cantores, compositores, intercessores e conselheiros de casais.</p>
   `,
+
   faleConosco: `
     <h1>Fale Conosco</h1>
     <form id="form-contato">
